efadf324a96a05179b96bc1063f3438c6480d2e3
nihalwashere <nihal.kaul@sjsu.edu>
2023-07-19 11:30:45 -0700

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 const title = "Nihal Kaul";
 const description =
   "Nihal is a software engineer who specializes in building exceptional digital experiences.";
+const url = "https://nihalwashere.xyz/";
+const image = "/about.png";
 
 export const metadata = {
   title,
@@ -18,9 +20,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/icon.png" type="image/png" sizes="any" />
+
+        <title>{title}</title>
+
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/about.png" />
+        <meta property="og:image" content={image} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
       </head>
 
       <body>{children}</body>
